67b3ef25d333b4b228d45a6af4d9f5caeeccd990
Smodger <chrisnorman1988@hotmail.co.uk>
2016-10-10 11:40:43 +0100

    creating maze board, fixed points. Need to add  timer and bonus tiles

--- js/app.js
+++ js/app.js
@@ -1,43 +1,76 @@
 $(function(){
 
-//INITIALISE
-//need to add placeholder, 'click here to start'
-//need to make it so clickEvent only can happen once
-  $(".tile").click(function(clickEvent){
-    var targetId = clickEvent.target.id;
-
-    $("#" + targetId).css("background-color", "yellow");
-    console.log("snap", clickEvent);
-  });
+  var $player = $('.player');
+  var $tiles = $('li');
 
 
+  var gridLength = 33;
 
-  var gridLength = 3; //will need to make this adaptive rather than fixed
   var moves = 0; //move counter
-  var currentTile = document.getElementById("t5");
-  //var $currentTile = $("#t5");
 
-  function movement () {
-    document.addEventListener("keydown", function (keyboardEvent) {
-      console.log(keyboardEvent);
-      if (keyboardEvent.code === "ArrowDown") {
-        (currentTile + gridLength).css("background-color", "yellow");
-        moves ++;
-      } else if
-      (keyboardEvent.code === "ArrowUp") {
-        (currentTile - gridLength).css("background-color", "yellow");
-        moves ++;
-      } else if
-      (keyboardEvent.code === "ArrowLeft") {
-        (currentTile --).css("background-color", "yellow");
-        moves ++;
-      } else if
-      (keyboardEvent.code === "ArrowRight") {
-        (currentTile ++).css("background-color", "yellow");
-        moves ++;
-      }
-    });
+
+  function movePlayer(keyboardEvent) {
+    // console.log(keyboardEvent.keyCode);
+    var index = $player.index();
+    switch(keyboardEvent.keyCode) {
+    case 38:
+      index -= gridLength;
+      break;
+    case 40:
+      index += gridLength;
+      break;
+    case 37:
+      index--;
+      break;
+    case 39:
+      index++;
+      break;
+    }
+
+//Index % gridLength below will change based on grid grid size, when grid is extended will need to update
+    if($tiles.eq(index).hasClass('wall') || $tiles.eq(index).hasClass('topWall') || $tiles.eq(index).hasClass('bottomWall') || $tiles.eq(index).hasClass('leftWall') || $tiles.eq(index).hasClass('rightWall')) {
+      return false;
+    }
+    //BONUS
+  //   if ($tiles.eq(index).hasClass('points'));
+  //   function myScore() {
+  //     points += 10;
+  //     function display() {
+  //       $("#score").show();
+  //       $("#score").html("Score: " + totalScore);
+  // }
+
+    //WIN
+    if ($tiles.eq(index).hasClass('win')) {
+      clearInterval(int);
+      alert("Congratulations you solved the maze!");
+      location.reload();
+    }
+    $player.removeClass('player');
+    $player = $tiles.eq(index).addClass('player');
   }
-  movement();
+
+  $(document).on('keydown', movePlayer);
+
+
+
+  //timer
+
+  //Score
+  var points = 60;
+  var decrease = 1;
+  var int;
+
+  function countDown(i, callback) {
+      callback = callback || function(){};
+      int = setInterval(function() {
+        $("#score").html("Score: " + i);
+          i-- || (clearInterval(int), callback());
+      }, 1000);
+  }
+      countDown(points, function(){
+          alert("Countdown done!");
+      });
+
 
 });
